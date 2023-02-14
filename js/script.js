05ceabbcbfefab9cca1127b8b19525101949dec0
Guilherme Oliveira <guilhermeoc.developer@gmail.com>
2023-02-14 11:44:56 -0300

    ADD: Adicionando projeto catálogo em fase final

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function show(id) {
+    if(document.getElementById(id).style.display == "block"){
+     document.getElementById(id).style.display = "none";
+    }
+     else{
+       document.getElementById(id).style.display = "block";
+     }
+    }
